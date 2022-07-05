15b44f4fa6c29acd44acb1765ea4c0c3ae4da670
ISRAEL OYEKAN <israel.f.oyekan@students.cookman.edu>
2022-07-05 17:06:23 +0000

    First commit for user input

---
+++ userinput.py
@@ -0,0 +1,3 @@
+# This is used to accept the user inputs and will be eventually stored in a database.
+# Save name and phone number once. It'll be used for notifications for the user.
+# Inputs needed: Name, Phone number, email(optional), food items bought.
