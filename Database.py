087db55616f8752ef99875e3b520facf3c63e32c
Luis Pagan <stemluis@gmail.com>
2022-07-07 03:47:19 +0000

    Added functionality

--- Database.py
+++ Database.py
@@ -5,38 +5,46 @@ import pandas as pd
 from datetime import datetime
 
 def deleteExistingDatabase():
-    myfile = "food_saver.db"
+    myfile = "Notifood.db"
     if os.path.isfile(myfile):
         os.remove(myfile)
 
 
 def setupDatabase():
     """Creates a database with necessary structure for program"""
-    engine = db.create_engine('sqlite:///food_saver.db')
-    engine.execute( "CREATE TABLE food_items(" + 
-                    " id INTEGER PRIMARY KEY AUTOINCREMENT," + 
-                    " purchase_date DATE," + 
-                    " expiration_date DATE," + 
-                    " item_name VARCHAR(100)," + 
-                    " item_category VARCHAR(40));")
+    engine = db.create_engine('sqlite:///Notifood.db')
+    if len(engine.table_names()) < 2:
+        engine.execute( 
+            """
+                CREATE TABLE food_items(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    purchase_date DATE,
+                    expiration_date DATE,
+                    item_name VARCHAR(100),
+                    item_category VARCHAR(40)
+                );
+            """
+        )
     return engine
 
 
 def addFoodItem(engine, name, category, purchase_date, expiration_date):
     """Adds a food item to the database"""
-    time_difference =   (datetime.strptime(expiration_date, '%Y-%m-%d') - 
-                        datetime.strptime(purchase_date, '%Y-%m-%d')).days
-    engine.execute( "INSERT INTO food_items(purchase_date, " +
-                    "expiration_date, item_name, item_category) " + 
-                    f"VALUES(\'{purchase_date}\', \'{expiration_date}\'," +
-                    f"\"{name}\",\"{category}\");")
-
-
-# def sortTable(engine, table_name):
-#     """Sorts the table based on food duration"""
-#     engine.execute( f"SELECT duration " +
-#                     f"FROM {table_name} " +
-#                     "ORDER BY duration DESC;")
+    engine.execute(
+        f"""
+            INSERT INTO food_items(
+                purchase_date,
+                expiration_date,
+                item_name,
+                item_category)
+            VALUES(
+                \'{purchase_date}\',
+                \'{expiration_date}\',
+                \'{name}\',
+                \'{category}\'
+            );
+        """
+    )
 
 
 def printDatabase(engine, query):
@@ -45,18 +53,26 @@ def printDatabase(engine, query):
     print(pd.DataFrame(query_result))
 
 
-def getFoodToExpire():
-    current_date = datetime.datetime.now()
+def getFoodToExpire(engine):
+    """Searches database and returns items set to expire within a week"""
+    query = engine.execute(
+        """
+            SELECT item_name, expiration_date FROM food_items
+            WHERE julianday(expiration_date) - julianday('now') < 7;
+        """
+    )
+    print(pd.DataFrame(query))
     
 
-
 def main():
-    deleteExistingDatabase()
+    # deleteExistingDatabase()
     data = setupDatabase();
-    addFoodItem(data, "Broccoli", "Vegetables", "2020-04-12", "2020-05-10")
-    addFoodItem(data, "Strawberry", "Fruits", "2020-08-12", "2020-08-20")
-    # sortTable(data, "food_items")
-    printDatabase(data, "food_items")
+    addFoodItem(data, "Broccoli", "Vegetables", "2022-07-5", "2022-07-09")
+    addFoodItem(data, "Strawberry", "Fruits", "2022-07-5", "2022-07-11")
+    addFoodItem(data, "Lettuce", "Vegetables", "2022-07-5", "2022-07-09")
+    addFoodItem(data, "Beef", "Meats", "2022-07-5", "2022-07-11")
+    getFoodToExpire(data)
+    # printDatabase(data, "food_items")
 
 if __name__ == "__main__":
     main()
